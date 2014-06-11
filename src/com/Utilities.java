17a8792698d35af08252b96aa187fbc6a23015b3
Robert Gerdisch <rgerdisch@gmail.com>
2014-06-11 12:10:55 -0400

    Added LCM function

--- src/com/Utilities.java
+++ src/com/Utilities.java
@@ -31,4 +31,40 @@ public class Utilities {
 			return a;
 		return GCD(b, a % b);
 	}
+	
+	/**
+	 * Returns the least common multiple from a list of integers, typically two.
+	 * To call the method, list any number of arguments. For example:
+	 * LCM(4, 5, 6, 8) or just LCM(4, 5)
+	 * @param nums the numbers to use to find the lcm
+	 * @return the least common multiple from a list of integers.
+	 */
+	public static int LCM(int... nums) {
+		if(nums.length == 1)
+			return nums[0];
+		
+		int[] _nums = nums.clone();
+		
+		int answer = 0;
+		boolean done = false;
+		while(!done) {
+			done = true;
+			int least = nums[0];
+			int least_index = 0;
+			for(int i = 1; i < nums.length; i++) {
+				if(nums[i] != nums[0])
+					done = false;
+				if(nums[i] < least) {
+					least_index = i;
+					least = nums[i];
+				}
+			}
+			if(!done)
+				nums[least_index] += _nums[least_index];
+			
+			answer = least;
+		}
+		
+		return answer;
+	}
 }
