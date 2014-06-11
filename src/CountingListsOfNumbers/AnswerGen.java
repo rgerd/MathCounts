8cc3915ecef820d2c5d9a2d48d2ff0de37b39ef4
Angela Li <angelali@angimacbookpro.parktudor.org>
2014-06-11 11:30:18 -0400

    AnswerGen ACTUALLY finished now
    CAVEN DOES IT IN THE BRASS

--- src/CountingListsOfNumbers/AnswerGen.java
+++ src/CountingListsOfNumbers/AnswerGen.java
@@ -1,5 +1,30 @@
 package CountingListsOfNumbers;
 
-public class AnswerGen {
+import java.util.HashSet;
 
+public class AnswerGen 
+{
+	private HashSet<Integer> h;
+	private int ans;
+	
+	public AnswerGen(int answer)
+	{
+		ans = answer;
+	}
+	
+	public void getWrong(String type)
+	{
+		if(type.equals("window"))
+		{
+			h.add(ans+1);
+			h.add(ans+2);
+			h.add(ans-1);
+			h.add(ans-2);
+		}
+	}
+	
+	public HashSet<Integer> getHash()
+	{
+		return h;
+	}
 }
