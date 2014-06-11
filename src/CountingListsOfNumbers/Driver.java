394a2cc3d1498de9af4179f5732b6adea19305f1
Amanda Li <adli2016@Amandas-MacBook-Air.local>
2014-06-11 11:58:43 -0400

    Driver Changed
    lalal

--- src/CountingListsOfNumbers/Driver.java
+++ src/CountingListsOfNumbers/Driver.java
@@ -1,5 +1,13 @@
 package CountingListsOfNumbers;
 
-public class Driver {
-
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		 Context con = new Context();
+		 con.generate(1);
+		 Question quest = new Question();
+		 String q = quest.getQ(con);
+		 System.out.println(q);
+	}
 }
