8bf97d66a1cb6136eae816b06cd3b55eea830f50
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-11 11:30:59 -0400

    Worked on Context

--- src/CountingListsOfNumbers/Context.java
+++ src/CountingListsOfNumbers/Context.java
@@ -1,5 +1,44 @@
 package CountingListsOfNumbers;
 
-public class Context {
+import java.util.ArrayList;
+import com.Utilities;
+import com.Fraction;
 
+public class Context<E> {
+	private int type;
+	private int length;
+	private ArrayList<Integer> list;
+	private int start;
+	
+	
+	public Context()
+	{
+		length=Utilities.getRandomNumberInRange(1,1000);
+		start=Utilities.getRandomNumberInRange(1,1000);
+		list = new ArrayList<E>();
+	}
+	
+	public void generate(int type)
+	{
+		
+	}
+	
+	public ArrayList<E> getList()
+	{
+		
+	}
+	
+	public E getAns() 
+	{
+		if(type == 1)
+		{
+			
+		}
+	}
+	
+	private void listGen1()
+	{
+		list.add(start);
+		list.
+	}
 }
