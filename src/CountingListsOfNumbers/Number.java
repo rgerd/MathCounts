f21521c1164392078994528caa2bba1c10de6387
Angela Li <angelali@angimacbookpro.parktudor.org>
2014-06-12 09:48:04 -0400

    Number Interface Added
    cheers

---
+++ src/CountingListsOfNumbers/Number.java
@@ -0,0 +1,9 @@
+package CountingListsOfNumbers;
+
+public interface Number 
+{
+	public Number add(Number n);
+	public Number sub(Number n);
+	public Number div(Number n);
+	public Number mult(Number n);
+}
