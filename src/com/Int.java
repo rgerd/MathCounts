188977c1c5e2196ff19cd307eb581643da2c5f90
Robert Gerdisch <rgerdisch@gmail.com>
2014-06-12 10:01:31 -0400

    Added Int

---
+++ src/com/Int.java
@@ -0,0 +1,72 @@
+package com;
+
+public class Int implements Number {
+	private int value;
+
+	/**
+	 * Creates a new integer with a value.
+	 * 
+	 * @param val
+	 *            the value of the integer.
+	 */
+	public Int(int val) {
+		value = val;
+	}
+
+	/**
+	 * Adds another integer to this integer.
+	 * 
+	 * @return the result.
+	 */
+	public Number add(Number n) {
+		Int _n = (Int) n;
+		return new Int(_n.value + value);
+	}
+
+	/**
+	 * Subtracts another integer from this integer.
+	 * 
+	 * @return the result.
+	 */
+	public Number sub(Number n) {
+		Int _n = (Int) n;
+		return new Int(value - _n.value);
+	}
+
+	/**
+	 * Divides this integer with another integer.
+	 * 
+	 * @return the result.
+	 */
+	public Number div(Number n) {
+		Int _n = (Int) n;
+		return new Int(value / _n.value);
+	}
+
+	/**
+	 * Multiplies this integer with another integer.
+	 * 
+	 * @return the result.
+	 */
+	public Number mult(Number n) {
+		Int _n = (Int) n;
+		return new Int(value * _n.value);
+	}
+
+	/**
+	 * Returns the value of this integer.
+	 * 
+	 * @return the value of this integer.
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * Returns a string representation of this integer.
+	 */
+	public String toString() {
+		return Integer.toString(value);
+	}
+
+}
