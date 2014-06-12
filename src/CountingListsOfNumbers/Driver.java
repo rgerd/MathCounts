66b80dd6b4a5518c3b3d9378ed18c8d49b2ceb9b
qqrocks <qqpetro@gmail.com>
2014-06-12 10:49:43 -0400

    SYNCK MY DRIVER STCHUFF
    you are a nerd

--- src/CountingListsOfNumbers/Driver.java
+++ src/CountingListsOfNumbers/Driver.java
@@ -23,17 +23,27 @@ public class Driver
 			output = new PrintWriter(new FileWriter(f));
 			for (int i = 0; i < 10; i++) {
 				 Context con = new Context();
-				 con.generate(1);
+				 con.generate(2);
 				 Question quest = new Question();
 				 String q = quest.getQ(con);
-				 System.out.println("MC"+"\t"+q);
+				 output.print("MC"+"\t"+q+"\t");
 				 
 				 int ans = con.getAns();
 				 AnswerGen a = new AnswerGen(ans);
 				 ArrayList<Number> arr = new ArrayList<Number>();
 				 arr = a.getWrong("window");
-				 System.out.println(arr);
-				
+				int r= Utilities.getRandomNumberInRange(1, 5);
+				 if(r==1)
+					 output.println(arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect");
+				 else if(r==2)
+					 output.println(arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect");
+				 else if(r==3)
+					 output.println(arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect");
+				 else if(r==4)
+					 output.println(arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect");
+				 else
+					 output.println(arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct");
+					
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
