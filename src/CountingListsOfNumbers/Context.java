5bcb42fc2796af52dc4dddd6cee2d190b0a0ee7f
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-13 10:11:36 -0400

    One Direction

--- src/CountingListsOfNumbers/Context.java
+++ src/CountingListsOfNumbers/Context.java
@@ -282,7 +282,7 @@ public class Context {
 			list.add(new Int ((int)(Math.pow(start+1,2))));
 			list.add(new Int ((int)(Math.pow(start+2,2))));
 			list.add(new Int ((int)(Math.pow(start+3,2))));
-			list.add(new Int ((int)(Math.pow(length-1,2))));
+			list.add(new Int ((int)(Math.pow(start + length-1,2))));
 
 		}else{
 			//decreasing
@@ -292,18 +292,8 @@ public class Context {
 			list.add(new Int ((int)(Math.pow(start-1,2))));
 			list.add(new Int ((int)(Math.pow(start-2,2))));
 			list.add(new Int ((int)(Math.pow(start-3,2))));
-			list.add(new Int ((int)(Math.pow(start-length-1,2))));
+			list.add(new Int ((int)(Math.pow(start-(length-1),2))));
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		
 	}
 	
