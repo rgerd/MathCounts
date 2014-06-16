81ec5b7befd79b11925280553de0133876149047
qqrocks <qqpetro@gmail.com>
2014-06-16 11:11:12 -0400

    blargh
    pls woirk

---
+++ src/CountingWithAdditionAndSubtraction/Driver.java
@@ -0,0 +1,80 @@
+package CountingWithAdditionAndSubtraction;
+
+import java.io.File;
+import util.Number;
+import util.Int;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import util.Utilities;
+
+public class Driver 
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Scanner in = new Scanner(new File("input1.txt"));
+		PrintWriter output = null;
+		try {
+			File f = new File("output.txt");
+			output = new PrintWriter(new FileWriter(f));
+			while(in.hasNextLine())
+			{
+				 String[] s = in.nextLine().split("\t");
+				 
+				 ArrayList<Integer> nums = new ArrayList<Integer>();
+				 ArrayList<Integer> types = new ArrayList<Integer>();
+				 for(int i = 2; i < s.length; i++)
+				 {
+					 String[] t = s[i].split(" ");
+					 nums.add(Integer.parseInt(t[0]));
+					 String ty = t[1];
+					 int type = Integer.parseInt(ty.substring(1, ty.length()));
+					 types.add(type);
+				 }
+				 
+				 for(int j = 0; j < types.size(); j++)
+				 {
+					 int typee = types.get(j);
+					 for(int k = 0; k < nums.get(j); k++)
+					 {
+						 Context con = new Context();
+						 con.generate(typee);
+						 Question quest = new Question();
+						 String q = quest.getQ(con);
+						 output.print("MC"+"\t"+q+"\t");
+						 
+						 int ans = con.getAns();
+						 AnswerGen a = new AnswerGen(ans);
+						 ArrayList<Number> arr = new ArrayList<Number>();
+						 arr = a.getWrong("window");
+						 
+						 int r= Utilities.getRandomNumberInRange(1, 5);
+						 if(r==1)
+							 output.println(arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect");
+						 else if(r==2)
+							 output.println(arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect");
+						 else if(r==3)
+							 output.println(arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect");
+						 else if(r==4)
+							 output.println(arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect");
+						 else
+							 output.println(arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct");
+					 }
+				 }
+					
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			output.close();
+		}
+
+	}
+		
+	}
+
---
+++ src/CountingWithAdditionAndSubtraction/Question.java
@@ -0,0 +1,53 @@
+package CountingWithAdditionAndSubtraction;
+
+import util.Utilities;
+
+
+public class Question {
+	private static final String Q = "How many numbers are in the list:";
+	private static final String Q_T14 = "How many positive numbers under ";
+	private static final String R = "How many roots between 1 and 10 inclusive are not included in the list";
+	private static final String M = "How Many numbers are multiples of";
+	private static final String Q_TYPE17 = "How many pairs of consecutive integers have products less than ";
+	private int length;
+	
+	public String getQ (Context c)
+	{
+		if(c.getType() == 17)
+		{
+			String str = Q_TYPE17 + c.getType17() + "?";
+			return str;
+		}else if(c.getType() == 14 || c.getType() == 13)
+		{
+			String str = Q_T14 + c.getList().get(0) + " are multiples of " + c.getList().get(1) + " but not " + c.getList().get(2) + "?";
+			return str;
+		}
+		else
+		{
+		String st = c.getList().toString();
+		int y = 0;
+		
+		for(int x =0; x<st.length();x++)
+		{
+			if(st.substring(x,x+1).equals(","))
+			{
+				y++;
+			}
+			if(y==4)
+			{
+				
+				st=" "+st.substring(1,x)+"..."+st.substring(x+1,st.length()-1)+"?";
+				
+				x = st.length();
+			}
+		}
+		String returnstring;
+		
+		returnstring = Q + st;
+		
+		return returnstring;
+		}
+	
+	}
+	
+}
