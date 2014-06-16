5d5ac3481230ee56e4c0b1ebbf8cc4eabb0bbe57
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-16 11:43:46 -0400

    Hunger Gamez

--- src/CountingMultipleEvents/Driver.java
+++ src/CountingMultipleEvents/Driver.java
@@ -1,10 +1,5 @@
 package CountingMultipleEvents;
 
-<<<<<<< HEAD
-public class Driver {
-
-}
-=======
 import java.io.File;
 
 import util.Number;
@@ -30,49 +25,49 @@ public class Driver
 			output = new PrintWriter(new FileWriter(f));
 			while(in.hasNextLine())
 			{
-				 String[] s = in.nextLine().split("\t");
-				 
-				 ArrayList<Integer> nums = new ArrayList<Integer>();
-				 ArrayList<Integer> types = new ArrayList<Integer>();
-				 for(int i = 2; i < s.length; i++)
-				 {
-					 String[] t = s[i].split(" ");
-					 nums.add(Integer.parseInt(t[0]));
-					 String ty = t[1];
-					 int type = Integer.parseInt(ty.substring(1, ty.length()));
-					 types.add(type);
-				 }
-				 
-				 for(int j = 0; j < types.size(); j++)
-				 {
-					 int typee = types.get(j);
-					 for(int k = 0; k < nums.get(j); k++)
-					 {
-						 Context con = new Context();
-						 con.generate(typee);
-						 Question quest = new Question();
-						 String q = quest.getQ(con);
-						 output.print("MC"+"\t"+q+"\t");
-						 
-						 int ans = con.getAns();
-						 AnswerGen a = new AnswerGen(ans);
-						 ArrayList<Number> arr = new ArrayList<Number>();
-						 arr = a.getWrong("window");
-						 
-						 int r= Utilities.getRandomNumberInRange(1, 5);
-						 if(r==1)
-							 output.println(arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect");
-						 else if(r==2)
-							 output.println(arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect");
-						 else if(r==3)
-							 output.println(arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect");
-						 else if(r==4)
-							 output.println(arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect");
-						 else
-							 output.println(arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct");
-					 }
-				 }
-					
+				String[] s = in.nextLine().split("\t");
+
+				ArrayList<Integer> nums = new ArrayList<Integer>();
+				ArrayList<Integer> types = new ArrayList<Integer>();
+				for(int i = 2; i < s.length; i++)
+				{
+					String[] t = s[i].split(" ");
+					nums.add(Integer.parseInt(t[0]));
+					String ty = t[1];
+					int type = Integer.parseInt(ty.substring(1, ty.length()));
+					types.add(type);
+				}
+
+				for(int j = 0; j < types.size(); j++)
+				{
+					int typee = types.get(j);
+					for(int k = 0; k < nums.get(j); k++)
+					{
+						Context con = new Context();
+						con.generate(typee);
+						Question quest = new Question();
+						String q = quest.getQ(con);
+						output.print("MC"+"\t"+q+"\t");
+
+						int ans = con.getAns();
+						AnswerGen a = new AnswerGen(ans);
+						ArrayList<Number> arr = new ArrayList<Number>();
+						arr = a.getWrong("window");
+
+						int r= Utilities.getRandomNumberInRange(1, 5);
+						if(r==1)
+							output.println(arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect");
+						else if(r==2)
+							output.println(arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect");
+						else if(r==3)
+							output.println(arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect");
+						else if(r==4)
+							output.println(arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct"+"\t"+arr.get(1)+"\t"+ "incorrect");
+						else
+							output.println(arr.get(1)+"\t"+ "incorrect"+"\t"+arr.get(2)+"\t"+ "incorrect"+"\t"+arr.get(3)+"\t"+ "incorrect"+"\t"+arr.get(4)+"\t"+ "incorrect"+"\t"+arr.get(0)+"\t"+ "correct");
+					}
+				}
+
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -81,7 +76,4 @@ public class Driver
 		}
 
 	}
-		
-	}
-
->>>>>>> origin/master
+}
