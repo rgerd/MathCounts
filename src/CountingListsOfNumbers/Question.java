d9a4de064caf98e6f299b6e401bdb60943a4a280
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-17 09:18:21 -0400

    Question changes
    Lol I don't know how this math works.

--- src/CountingListsOfNumbers/Question.java
+++ src/CountingListsOfNumbers/Question.java
@@ -4,43 +4,13 @@ import util.Utilities;
 
 
 public class Question {
-	private static final String Q = "How many numbers are in the list:";
-	private static final String Q_T14 = "How many positive numbers under ";
-	private static final String R = "How many roots between 1 and 10 inclusive are not included in the list";
-	private static final String M = "How Many numbers are multiples of";
-	private static final String Q_TYPE17 = "How many pairs of consecutive integers have products less than ";
-	private static final String Q_TYPE15 = "You are assigned homework problems from ";
-	private static final String Q_TYPE19 = "You are assigned odd homework problems from ";
-	private static final String Q_TYPE20 = "You are assigned even homework problems from ";
-	private int length;
+	private static final String Q = "You have ";
 	
 	public String getQ (Context c)
 	{
-		if(c.getType() == 17)
+		if(c.getType() == 1)
 		{
-			String str = Q_TYPE17 + c.getType17() + "?";
-			return str;
-		}else if(c.getType() == 14 || c.getType() == 13)
-		{
-			String str = Q_T14 + c.getList().get(0) + " are multiples of " + c.getList().get(1) + " but not " + c.getList().get(2) + "?";
-			return str;
-		}else if(c.getType() == 15)
-		{
-			String str = Q_TYPE15 + c.getList().get(0) + " to " + c.getList().get(1) + " (inclusive). How many problems were assigned?";
-			return str;
-		}else if (c.getType() == 19)
-		{
-			int chance = Utilities.getRandomNumberInRange(1, 2);
-			if(chance == 1)
-			{
-				String str = Q_TYPE19 + c.getList().get(0) + " to " + c.getList().get(1) + " (inclusive). How many problems were assigned?";
-				return str;
-			}
-			else
-			{
-				String str = Q_TYPE20 + c.getList().get(0) + " to " + c.getList().get(1) + " (inclusive). How many problems were assigned?";
-				return str;
-			}
+			
 		}
 		else
 		{
