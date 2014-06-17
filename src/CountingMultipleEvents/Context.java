0fca7b2fb6a838efad14ded1b7c12e2f04794a67
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-17 10:12:51 -0400

    Boyz II men

--- src/CountingMultipleEvents/Context.java
+++ src/CountingMultipleEvents/Context.java
@@ -76,7 +76,12 @@ public class Context {
 	{
 		String noun1 = "shirts";
 		String noun2 = "pants";
-		
+		int num1 = Utilities.getRandomNumberInRange(1,50);
+		int num2 = Utilities.getRandomNumberInRange(1,50);
+		list.add(noun1);
+		list.add(Integer.toString(num1));
+		list.add(noun2);
+		list.add(Integer.toString(num2));
 		/**Question format: You have (#1) (attribute #1)s and (#2) (attribute #2)s. How many ways can you choose a (collection) consisting of 1 (attribute #1) and 1 (attribute #2)?
 				Context: shirts and pants make an outfit
 				Variation #1: flavors + toppings on ice cream
