960b356585e353ca07454885a03b49cd6637f136
qqrocks <qqpetro@gmail.com>
2014-06-17 10:54:20 -0400

    afeiu

--- src/CountingMultipleEvents/Question.java
+++ src/CountingMultipleEvents/Question.java
@@ -17,7 +17,14 @@ public class Question {
 			String ret = Q1 +  list.get(0) + " " + list.get(1) + "(s)  and " + list.get(2) + " " + list.get(3) + "(s). How many ways can you choose a(n) "  + list.get(4) +  " consisting of 1 " + list.get(1) + " and 1 " + list.get(3) + "?";
 			return ret;
 		}
-		
+		else if(c.getType()==2)
+		{
+			
+		}
+		else if(c.getType()==3)
+		{
+			
+		}
 		return  ret;
 			
 	}
