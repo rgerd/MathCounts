0524800d1c3a489fbcab0696deb8de818859f083
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-17 10:34:15 -0400

    98 Degrees

--- src/CountingMultipleEvents/Context.java
+++ src/CountingMultipleEvents/Context.java
@@ -57,11 +57,8 @@ public class Context {
 		list.add(Integer.toString(num1));
 		list.add(noun2);
 		list.add(Integer.toString(num2));
-<<<<<<< HEAD
 		System.out.println("called");
-=======
 		ans = num1 * num2;
->>>>>>> FETCH_HEAD
 		/**Question format: You have (#1) (attribute #1)s and (#2) (attribute #2)s. How many ways can you choose a (collection) consisting of 1 (attribute #1) and 1 (attribute #2)?
 				Context: shirts and pants make an outfit
 				Variation #1: flavors + toppings on ice cream
