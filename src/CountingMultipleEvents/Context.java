361579cd7f76c8bfc8ee8369dcc4c0d0671461ed
Angela Li <angelali@angimacbookpro.parktudor.org>
2014-06-18 10:14:20 -0400

    AHHH

--- src/CountingMultipleEvents/Context.java
+++ src/CountingMultipleEvents/Context.java
@@ -230,7 +230,35 @@ public class Context {
 		Example = Basketball - 1 center, 1 power forward, 1 shooting forward, 1 point guard, 1 shooting guard
 		Do other sports**/
 
-		chance4 = Utilities.getRandomNumberInRange(1, 5);
+		chance4 = Utilities.getRandomNumberInRange(1, 6);
+		ArrayList<String> alphabet = new ArrayList<String>();
+		alphabet.add("A");
+		alphabet.add("B");
+		alphabet.add("C");
+		alphabet.add("D");
+		alphabet.add("E");
+		alphabet.add("F");
+		alphabet.add("G");
+		alphabet.add("H");
+		alphabet.add("I");
+		alphabet.add("J");
+		alphabet.add("K");
+		alphabet.add("L");
+		alphabet.add("M");
+		alphabet.add("N");
+		alphabet.add("O");
+		alphabet.add("P");
+		alphabet.add("Q");
+		alphabet.add("R");
+		alphabet.add("S");
+		alphabet.add("T");
+		alphabet.add("U");
+		alphabet.add("V");
+		alphabet.add("W");
+		alphabet.add("X");
+		alphabet.add("Y");
+		alphabet.add("Z");
+		
 		if(chance4 == 1)
 		{
 			String s = "shirts";
@@ -252,6 +280,9 @@ public class Context {
 		{
 			
 		}else if(chance4 == 4)
+		{
+			
+		}else if(chance4 == 5)
 		{
 			
 		}else
