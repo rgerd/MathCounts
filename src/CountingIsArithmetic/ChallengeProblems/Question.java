d09236c81567ca18512922cd4856b9c503d4f296
Robert Gerdisch <rgerdisch@gmail.com>
2014-06-19 11:54:40 -0400

    USE POPULATE

--- src/CountingIsArithmetic/ChallengeProblems/Question.java
+++ src/CountingIsArithmetic/ChallengeProblems/Question.java
@@ -27,8 +27,7 @@ public class Question extends com.Question{
 		}
 		if(c.getType() == 4)
 		{
-			String zimzambabityboopity=("My house has " +c.getList().get(0)+ "herds of cats, with "+c.getList().get(1) + "cats in each herd. The cats in each herd are numbered from 1 to"+c.getList().get(1)+". How many cats have odd numbers?");
-			return zimzambabityboopity;
+			return LangUtility.populate("My house has [num_0] herds of cats, with [num_1] cats in each herd. The cats in each herd are numbered from 1 to [num_1]. How many cats have odd numbers?", c.getList());
 		}
 
 		return null;
