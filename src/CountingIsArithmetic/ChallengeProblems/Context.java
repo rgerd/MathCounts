92a034a79df9f0351dcf32f8d2335c70c573a638
Angela Li <angelali@angimacbookpro.parktudor.org>
2014-06-19 11:20:43 -0400

    worked on gen3

--- src/CountingIsArithmetic/ChallengeProblems/Context.java
+++ src/CountingIsArithmetic/ChallengeProblems/Context.java
@@ -1,8 +1,10 @@
 package CountingIsArithmetic.ChallengeProblems;
 
 import java.util.ArrayList;
+
 import util.Utilities;
 import util.Number;
+import util.Int;
 
 public class Context extends com.Context{
 	private int ans;
@@ -50,14 +52,30 @@ public class Context extends com.Context{
 	
 	private void gen3()
 	{
-		
-		
-		
-		
-		
-		
-		
-		
+		/**Type 03: Math with factorials
+		What is the greatest common factor of (#1)!, (#2)!, and (#3)!?
+		What is the units digit of 1! + 2! + 3! +4! + É +(#1)!?
+		How many of the factorials from (#1)! to (#2)! are divisible by (#3)?
+		 **/
+		chance = Utilities.getRandomNumberInRange(1, 3);
+		if(chance == 1)
+		{
+			int num1 = Utilities.getRandomNumberInRange(10, 100);
+			int num2 = Utilities.getRandomNumberInRange(10, 100);
+			int num3 = Utilities.getRandomNumberInRange(10, 100);
+			
+			list.add(new Int(num1));
+			list.add(new Int(num2));
+			list.add(new Int(num3));
+			
+			
+		} else if(chance == 2)
+		{
+			
+		} else if(chance == 3)
+		{
+			
+		}
 		
 	}
 	
--- src/CountingIsArithmetic/ChallengeProblems/Question.java
+++ src/CountingIsArithmetic/ChallengeProblems/Question.java
@@ -5,7 +5,8 @@ import com.Context;
 public class Question extends com.Question{
 
 	@Override
-	public String getQ(com.Context c) {
+	public String getQ(com.Context c) 
+	{	
 		Context c = (Context) _c;
 	}
 
