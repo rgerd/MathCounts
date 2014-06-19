9ed3941ab0093d04eb30794f7c4d42cdc09b2cb2
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-19 10:46:25 -0400

    look at all dem lines

--- src/CountingIsArithmetic/ChallengeProblems/Context.java
+++ src/CountingIsArithmetic/ChallengeProblems/Context.java
@@ -1,5 +1,96 @@
 package CountingIsArithmetic.ChallengeProblems;
 
-public class Context {
+import java.util.ArrayList;
 
+import util.Number;
+
+public class Context extends com.Context{
+	private int ans;
+	private ArrayList<Number> list;
+	
+	public Context()
+	{
+		
+	}
+
+	public ArrayList<String> getList() {
+		return list;
+	}
+
+	public int getAns() {
+		return ans; 
+	}
+	
+	private void gen1()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	private void gen2()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	private void gen3()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	private void gen4()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	private void gen5()
+	{
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
