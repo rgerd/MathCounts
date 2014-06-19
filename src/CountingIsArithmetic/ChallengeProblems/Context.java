5d1a488649d8cc4ba0b66a102e11ce735a954ac7
Robert Gerdisch <rgerdisch@gmail.com>
2014-06-19 11:35:31 -0400

    Working on type 5

--- src/CountingIsArithmetic/ChallengeProblems/Context.java
+++ src/CountingIsArithmetic/ChallengeProblems/Context.java
@@ -1,17 +1,19 @@
 package CountingIsArithmetic.ChallengeProblems;
 
 import java.util.ArrayList;
+
+import util.Fraction;
+import util.Int;
 import util.Utilities;
 import util.Number;
 
-public class Context extends com.Context{
+public class Context extends com.Context {
 	private int ans;
 	private ArrayList<Number> list;
 	private int chance;
-	
-	public Context()
-	{
-		
+
+	public Context() {
+		list = new ArrayList<Number>();
 	}
 
 	public ArrayList<Number> getList() {
@@ -19,68 +21,43 @@ public class Context extends com.Context{
 	}
 
 	public int getAns() {
-		return ans; 
+		return ans;
 	}
-	
-	private void gen1()
-	{
-		int length = Utilities.getRandomNumberInRange(10,1000);
-		int numSquares = Utilities.getRandomNumberInRange(2,8);
-		
-		
-		
-		
-		
-		
-		
+
+	private void gen1() {
+		int length = Utilities.getRandomNumberInRange(10, 1000);
+		int numSquares = Utilities.getRandomNumberInRange(2, 8);
+
 	}
-	
-	private void gen2()
-	{
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	private void gen2() {
+
 	}
-	
-	private void gen3()
-	{
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	private void gen3() {
+
 	}
-	
-	private void gen4()
-	{
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+	private void gen4() {
+
 	}
-	
-	private void gen5()
-	{
-		//How many integers n satisfy (#1 / #2) < 1 / n < (#3 / #4)?
-		
+
+	private void gen5() {
+
+		int num_0 = Utilities.getRandomNumberInRange(0, 1000);
+		int den_0 = Utilities.getRandomNumberInRange(0, 1000);
+
+		int num_1 = Utilities.getRandomNumberInRange(0, 1000);
+		int den_1 = Utilities.getRandomNumberInRange(0, 1000);
+
+		Fraction frac_0 = new Fraction(num_0, den_0);
+		Fraction frac_1 = (Fraction) new Fraction(num_1, den_1).add(frac_0);
+		list.add(frac_0);
+		list.add(frac_1);
+
+		int top = (int) frac_1.reciprocal().divide();
+		int bottom = (int) frac_0.reciprocal().divide();
+
+		list.add(new Int(top - bottom + 1));
 	}
 }
--- src/CountingIsArithmetic/ChallengeProblems/Question.java
+++ src/CountingIsArithmetic/ChallengeProblems/Question.java
@@ -1,10 +1,14 @@
 package CountingIsArithmetic.ChallengeProblems;
 
+import util.lang.LangUtility;
+
 public class Question extends com.Question{
 	public String getQ(com.Context _c) {
 		Context c = (Context) _c;
 		
-		
+		if(c.getType() == 5) {
+			return LangUtility.populate("How many integers n satisfy ([num_0]) < 1 / n < ([num_1])?", c.getList());
+		}
 		
 		return null;
 	}
