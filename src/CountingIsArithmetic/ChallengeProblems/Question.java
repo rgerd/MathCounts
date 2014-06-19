9fe3fa3bc97ba9ff3bb61d9e559f440c802b4c67
qqrocks <qqpetro@gmail.com>
2014-06-19 11:35:54 -0400

    ewt
    wteeeeeeeeeeeeeeeeee

--- src/CountingIsArithmetic/ChallengeProblems/Question.java
+++ src/CountingIsArithmetic/ChallengeProblems/Question.java
@@ -12,10 +12,10 @@ public class Question extends com.Question{
 	{	
 
 	
-		Context c = (Context) _c;
+		
 		if(c.getType() == 1)
 		{
-			String question = LangUtility.populate("How many positive integers less than [num_0] can be written as the sum of [num_1] positive perfect squares.",);
+			String question = LangUtility.populate("How many positive integers less than [num_0] can be written as the sum of [num_1] positive perfect squares.");
 		}
 		
 		
