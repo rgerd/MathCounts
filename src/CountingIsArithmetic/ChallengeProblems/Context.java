3d49cae662bc87f938ffcc3c57b3543dba0609bd
Angela Li <angelali@angimacbookpro.parktudor.org>
2014-06-20 09:22:05 -0400

    lal

--- src/CountingIsArithmetic/ChallengeProblems/Context.java
+++ src/CountingIsArithmetic/ChallengeProblems/Context.java
@@ -24,22 +24,6 @@ public class Context extends com.Context {
 	public Number getAnswer() {
 		return ans;
 	}
-<<<<<<< HEAD
-	
-	private void gen1()
-	{
-		/**Question format: How many positive integers less than (#1) can be written as the
-		sum of (#2) positive perfect squares? **/
-		int length = Utilities.getRandomNumberInRange(10,1000);
-		int numSquares = Utilities.getRandomNumberInRange(2,8);
-		
-		
-		
-		
-		
-		
-
-=======
 
 	private void gen1() {
 		/**
@@ -56,36 +40,12 @@ public class Context extends com.Context {
 		for (int i = 0; i < leftovers; i++) {
 			int uppercombos = (upperLimit + i);
 		}
->>>>>>> FETCH_HEAD
 	}
 
 	private void gen2() {
 
 	}
 
-<<<<<<< HEAD
-	private void gen3()
-	{
-		/**Type 03: Math with factorials
-		What is the greatest common factor of (#1)!, (#2)!, and (#3)!?
-		What is the units digit of 1! + 2! + 3! +4! + É +(#1)!?
-		How many of the factorials from (#1)! to (#2)! are divisible by (#3)?
-		 **/
-		chance = Utilities.getRandomNumberInRange(1, 3);
-		chance =1;
-		if(chance == 1)
-		{
-			int num1 = Utilities.getRandomNumberInRange(10, 100);
-			int num2 = Utilities.getRandomNumberInRange(10, 100);
-			int num3 = Utilities.getRandomNumberInRange(10, 100);
-			
-			list.add(new Int(num1));
-			list.add(new Int(num2));
-			list.add(new Int(num3));
-			
-			int min = Math.min(num1, num2);
-			min = Math.min(num2, num3);
-=======
 	private void gen3() {
 		/**
 		 * Type 03: Math with factorials What is the greatest common factor of
@@ -94,7 +54,7 @@ public class Context extends com.Context {
 		 * divisible by (#3)?
 		 **/
 		chance = Util.getRandomNumberInRange(1, 3);
-		
+
 		if (chance == 1) {
 			int num1 = Util.getRandomNumberInRange(10, 100);
 			int num2 = Util.getRandomNumberInRange(10, 100);
@@ -105,25 +65,11 @@ public class Context extends com.Context {
 			list.add(new Factorial(num3));
 
 			int min = Math.min(num1, Math.min(num2, num3));
->>>>>>> FETCH_HEAD
-			
+
 			int answer = min;
 			for (int i = 1; i < min; i++) {
 				answer *= min - i;
 			}
-<<<<<<< HEAD
-			
-			ans = answer;
-			
-		} else if(chance == 2)
-		{
-			
-		} else if(chance == 3)
-		{
-			
-		}
-		
-=======
 
 			ans = new Int(answer);
 
@@ -132,18 +78,21 @@ public class Context extends com.Context {
 		} else if (chance == 3) {
 
 		}
->>>>>>> FETCH_HEAD
+
 	}
 
-	private void gen4() {
+	private void gen4() 
+	{
 		int answer = 0;
 
 		int g = Util.getRandomNumberInRange(2, 100);
 		int i = Util.getRandomNumberInRange(2, 100);
 		list.add(new Int(g));
 		list.add(new Int(i));
-		for (int x = 0; x < g; x++) {
-			for (int y = 0; y < i; y++) {
+		for (int x = 0; x < g; x++)
+		{
+			for (int y = 0; y < i; y++) 
+			{
 				if (i % 2 == 1)
 					answer++;
 			}
@@ -151,21 +100,22 @@ public class Context extends com.Context {
 		ans = new Int(answer);
 	}
 
-	private void gen5() {
-		int num_0 = Util.getRandomNumberInRange(1, 10);
-		int den_0 = Util.getRandomNumberInRange(1, 10);
 
-		int num_1 = Util.getRandomNumberInRange(1, 5);
-		int den_1 = Util.getRandomNumberInRange(5, 10);
+private void gen5() {
+	int num_0 = Util.getRandomNumberInRange(1, 10);
+	int den_0 = Util.getRandomNumberInRange(1, 10);
 
-		Fraction frac_0 = new Fraction(num_0, den_0);
-		Fraction frac_1 = (Fraction) new Fraction(num_1, den_1).add(frac_0);
-		list.add(frac_0);
-		list.add(frac_1);
+	int num_1 = Util.getRandomNumberInRange(1, 5);
+	int den_1 = Util.getRandomNumberInRange(5, 10);
 
-		int top = (int) frac_1.reciprocal().divide();
-		int bottom = (int) frac_0.reciprocal().divide();
+	Fraction frac_0 = new Fraction(num_0, den_0);
+	Fraction frac_1 = (Fraction) new Fraction(num_1, den_1).add(frac_0);
+	list.add(frac_0);
+	list.add(frac_1);
 
-		ans = new Int(Math.abs(top - bottom) + 1);
-	}
+	int top = (int) frac_1.reciprocal().divide();
+	int bottom = (int) frac_0.reciprocal().divide();
+
+	ans = new Int(Math.abs(top - bottom) + 1);
+}
 }
