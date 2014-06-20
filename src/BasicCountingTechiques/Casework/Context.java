3dda5bddc6c02f7d05f8a62bc7c67d32fa3e018b
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-20 11:17:50 -0400

    we're waiting

--- src/BasicCountingTechiques/Casework/Context.java
+++ src/BasicCountingTechiques/Casework/Context.java
@@ -1,5 +1,13 @@
 package BasicCountingTechiques.Casework;
 
+import util.Number;
+
 public class Context extends com.Context{
+
+	@Override
+	public Number getAnswer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/BasicCountingTechiques/Casework/Question.java
+++ src/BasicCountingTechiques/Casework/Question.java
@@ -5,14 +5,8 @@ import util.lang.LangUtil;
 import com.Context;
 
 public class Question extends com.Question {
+	private static String[] questions = new String[9];
 
-	questions[0] = "How many positive integers less than [num_0] can be written as the sum of 2 positive perfect squares?";
-	/* type 2 */ questions[1] = null;
-	/* type 3 */ questions[2] = "What is the greatest common factor of [num_0], [num_1], and [num_2] ?";
-	/* type 4 */ questions[3] = "My house has [num_0] [collection_0] of [noun_0], with [num_1] [noun_0] in each [collection_0_sng]. The [noun_0] in each [collection_0_sng] are numbered from 1 to [num_1]. How many [noun_0_pl] have odd numbers?";
-	/* type 5 */ questions[4] = "How many integers n satisfy ([num_0]) < 1 / n < ([num_1])?";
-	/*type 3.1*/ questions[5] = "What is the units digit of 1! + 2! + 3! + 4! + ... + [num_0] ?";
-	/*type 3.2*/ questions[6] = "How many of the factorials from [num_0] to [num_1] are divisible by [num_2]?";
 	public String getQ(com.Context _c) {
 		Context c = (Context) _c;
 		return LangUtil.populate(questions[c.getType() - 1], c.getList());
