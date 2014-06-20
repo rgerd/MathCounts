235cf565b67dff359e72453678665968806877d0
qqrocks <qqpetro@gmail.com>
2014-06-20 11:17:35 -0400

    RT^

--- src/BasicCountingTechiques/Casework/Context.java
+++ src/BasicCountingTechiques/Casework/Context.java
@@ -1,5 +1,58 @@
 package BasicCountingTechiques.Casework;
 
-public class Context {
+import java.util.ArrayList;
+import java.util.HashSet;
 
+import util.Factorial;
+import util.Fraction;
+import util.Int;
+import util.Number;
+import util.Util;
+
+public class Context extends com.Context {
+	private Number ans;
+	private ArrayList<Number> list;
+	private int chance;
+
+	public Context() {
+		list = new ArrayList<Number>();
+	}
+
+	public ArrayList<Number> getList() {
+		return list;
+	}
+
+	public Number getAnswer() {
+		return ans;
+	}
+
+	private void gen1() {
+	
+	}
+
+	private void gen2() {
+
+	}
+
+	private void gen3() {
+	
+	}
+
+	private void gen4() 
+	{
+		
+	}
+
+
+	private void gen5() {
+	
+	}
+
+	public int chance()
+	{
+		return chance;
+	}
 }
+
+
+
