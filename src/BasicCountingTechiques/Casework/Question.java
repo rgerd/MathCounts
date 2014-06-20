106edef1fc9799b397c143cbf61a43db5cdd39a3
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-20 11:20:31 -0400

    writing questions

--- src/BasicCountingTechiques/Casework/Question.java
+++ src/BasicCountingTechiques/Casework/Question.java
@@ -6,7 +6,8 @@ import com.Context;
 
 public class Question extends com.Question {
 	private static String[] questions = new String[9];
-
+	questions[1] = "The Mumblians decide to change their laws to only allow words that contain the letter A at least once. But they still limit their words to 3 letters or less, and they wish to use only letters AEMBN.";
+	
 	public String getQ(com.Context _c) {
 		Context c = (Context) _c;
 		return LangUtil.populate(questions[c.getType() - 1], c.getList());
