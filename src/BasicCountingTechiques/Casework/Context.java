6f65bc26d0634c1a43ebcf81a35e0f858ae0e418
Robert Gerdisch <rgerdisch@gmail.com>
2014-06-21 11:48:31 -0400

    Formatting

--- src/BasicCountingTechiques/Casework/Context.java
+++ src/BasicCountingTechiques/Casework/Context.java
@@ -1,9 +1,7 @@
 package BasicCountingTechiques.Casework;
 
-
 import java.util.ArrayList;
 import java.util.HashSet;
-
 
 import util.Factorial;
 import util.Fraction;
@@ -29,7 +27,7 @@ public class Context extends com.Context {
 	}
 
 	private void gen1() {
-		
+
 	}
 
 	private void gen2() {
@@ -37,33 +35,27 @@ public class Context extends com.Context {
 	}
 
 	private void gen3() {
-	
+
 	}
 
-	private void gen4() 
-	{
-		int p= Util.getRandomNumberInRange(5,100);
-		int p1= Util.getRandomNumberInRange(5,100);
-		int p2= Util.getRandomNumberInRange(5,100);
+	private void gen4() {
+		int p = Util.getRandomNumberInRange(5, 100);
+		int p1 = Util.getRandomNumberInRange(5, 100);
+		int p2 = Util.getRandomNumberInRange(5, 100);
 		list.add(new Int(p));
 		list.add(new Int(p1));
 		list.add(new Int(p2));
-		p*=p1;
-		p*=p2;
-		Int q=new Int(p);
-		ans=q;
+		p *= p1;
+		p *= p2;
+		Int q = new Int(p);
+		ans = q;
 	}
-
 
 	private void gen5() {
-	
+
 	}
 
-	public int chance()
-	{
+	public int chance() {
 		return chance;
 	}
 }
-
-
-
