ed4ee708c8722a5546a1633609c00c1bf4b700e4
Robert Gerdisch <rgerdisch@gmail.com>
2014-06-21 17:27:18 -0400

    Working on section 3
    (._.)

--- src/CountingIsArithmetic/CountingWithAdditionAndSubtraction/Context.java
+++ src/CountingIsArithmetic/CountingWithAdditionAndSubtraction/Context.java
@@ -6,57 +6,31 @@ import util.Number;
 import util.Util;
 
 public class Context extends com.Context {
-	private int length;
+	private Number answer;
 	private ArrayList<Number> list;
-	private int start;
 
 	public Context() {
 		list = new ArrayList<Number>();
+		answer = null;
 	}
 
 	public ArrayList<Number> getList() {
 		return list;
 	}
 
-	public Int getAnswer() {
-		if (getType() == 2) {
-			int count = 0;
-			for (int i = 0; i < list.size(); i++) {
-				if (i == 0) {
-					Int i1 = (Int) (list.get(i));
-					int i2 = i1.getValue();
-					System.out.println(i2);
-					count = +i2;
-				} else {
-					Int i1 = (Int) (list.get(i));
-					int i2 = i1.getValue();
-					System.out.println(i2);
-					count *= i2;
-				}
-			}
-			System.out.println(count);
-
-			return new Int(count);
-		}
-		return new Int(length);
-
-	}
-
 	private void gen1() {
-		Integer type1 = Util.getRandomNumberInRange(2, 15);
-		Integer type2 = Util.getRandomNumberInRange(2, 15);
-		Int init = new Int(start);
-		list.add(init);
-		list.add(init.add(new Int(type1)));
-		list.add(init.add(new Int(type2)));
+		Int total = new Int(Util.getRandomNumberInRange(10, 100));
+		Int num_0 = new Int(Util.getRandomNumberInRange((int) (total.getValue() * 0.1), (int) (total.getValue() * 0.9)));
+		Int num_1 = (Int) total.sub(num_0);
+		
+		list.add(total);
+		list.add(num_0);
+		list.add(num_1);
+		
+		answer = total.sub(num_0.add(num_1));
 	}
 
 	private void gen2() {
-		Integer types = Util.getRandomNumberInRange(3, 5);
-		Int init = new Int(start);
-		for (int i = 0; i < types; i++) {
-			list.add(init.add(new Int(Util.getRandomNumberInRange(2, 15))));
-		}
 	}
 
 	private void gen3() {
@@ -73,5 +47,13 @@ public class Context extends com.Context {
 
 	private void gen6() {
 
+	}
+
+	private void gen7() {
+
+	}
+
+	public Number getAnswer() {
+		return answer;
 	}
 }
--- src/CountingIsArithmetic/CountingWithAdditionAndSubtraction/Question.java
+++ src/CountingIsArithmetic/CountingWithAdditionAndSubtraction/Question.java
@@ -1,56 +1,16 @@
 package CountingIsArithmetic.CountingWithAdditionAndSubtraction;
 
+import util.lang.LangUtil;
 
 public class Question extends com.Question {
-	private static final String Q = "How many numbers are in the list:";
-	private static final String Q_T14 = "How many positive numbers under ";
-	private static final String R = "How many roots between 1 and 10 inclusive are not included in the list";
-	private static final String M = "How Many numbers are multiples of";
-	private static final String Q_TYPE17 = "How many pairs of consecutive integers have products less than ";
-	private int length;
+	String[] questions = {
+		"[venue_0], there are [num_0] [noun_0] that [verb_0] [adj_0] and [adj_1]. If [num_1] [noun_0] [verb_0] only [adj_0] and [num_1] [noun_0] [verb_0] only [adj_1], how many [noun_0_pl] must be both [adj_0] and [adj_1]?",	
+	};
 	
-	public String getQ (com.Context _c)
-	{
+	public String getQ(com.Context _c) {
+		
 		Context c = (Context) _c;
-		return null;
-		//Commented out until someone can fix the errors
-		/*
-		if(c.getType() == 17)
-		{
-			String str = Q_TYPE17 + c.getType17() + "?";
-			return str;
-		}else if(c.getType() == 14 || c.getType() == 13)
-		{
-			String str = Q_T14 + c.getList().get(0) + " are multiples of " + c.getList().get(1) + " but not " + c.getList().get(2) + "?";
-			return str;
-		}
-		else
-		{
-		String st = c.getList().toString();
-		int y = 0;
 		
-		for(int x =0; x<st.length();x++)
-		{
-			if(st.substring(x,x+1).equals(","))
-			{
-				y++;
-			}
-			if(y==4)
-			{
-				
-				st=" "+st.substring(1,x)+"..."+st.substring(x+1,st.length()-1)+"?";
-				
-				x = st.length();
-			}
-		}
-		String returnstring;
-		
-		returnstring = Q + st;
-		
-		return returnstring;
-		}
-		*/
+		return LangUtil.populate(questions[c.getType() - 1], c.getList());
 	}
-	
-
 }
