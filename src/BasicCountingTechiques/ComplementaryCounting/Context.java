d14afdea942ad9470651d15b4812232cc8ded0da
qqrocks <qqpetro@gmail.com>
2014-06-23 09:25:00 -0400

    MAKING NEW THINGS
    RT^
    RT^

---
+++ src/BasicCountingTechiques/ComplementaryCounting/Context.java
@@ -0,0 +1,13 @@
+package BasicCountingTechiques.ComplementaryCounting;
+
+public class Context {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/BasicCountingTechiques/ComplementaryCounting/Question.java
@@ -0,0 +1,13 @@
+package BasicCountingTechiques.ComplementaryCounting;
+
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
