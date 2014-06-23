6b083e66c849c0a720149b7b14b71704dec9c780
Angela Li <angelali@angimacbookpro.parktudor.org>
2014-06-23 11:24:19 -0400

    famirieeees

--- src/BasicCountingTechniques/Casework/Context.java
+++ src/BasicCountingTechniques/Casework/Context.java
@@ -61,7 +61,7 @@ public class Context extends com.Context {
 		int length = Util.getRandomNumberInRange(5, 10);
 		list.add(new Int(length));
 		
-		while(alph.length() < 5)
+		while(alph.length() < length)
 		{
 			int rand = Util.getRandomNumberInRange(0, 25);
 			if(!alph.contains(alphabet.get(rand)))
@@ -70,7 +70,7 @@ public class Context extends com.Context {
 		
 		list.add(new CustomNumber(alph));
 		
-		int numLetters = Util.getRandomNumberInRange(3, length);
+		int numLetters = Util.getRandomNumberInRange(2, 6);
 		list.add(new Int(numLetters));
 		
 		int answer = 1;
@@ -84,10 +84,66 @@ public class Context extends com.Context {
 	}
 
 	private void gen2() {
-
+		ArrayList<String> alphabet = new ArrayList<String>();
+		alphabet.add("A");
+		alphabet.add("B");
+		alphabet.add("C");
+		alphabet.add("D");
+		alphabet.add("E");
+		alphabet.add("F");
+		alphabet.add("G");
+		alphabet.add("H");
+		alphabet.add("I");
+		alphabet.add("J");
+		alphabet.add("K");
+		alphabet.add("L");
+		alphabet.add("M");
+		alphabet.add("N");
+		alphabet.add("O");
+		alphabet.add("P");
+		alphabet.add("Q");
+		alphabet.add("R");
+		alphabet.add("S");
+		alphabet.add("T");
+		alphabet.add("U");
+		alphabet.add("V");
+		alphabet.add("W");
+		alphabet.add("X");
+		alphabet.add("Y");
+		alphabet.add("Z");
+		
+		String alph = "";
+		
+		int length = Util.getRandomNumberInRange(5, 10);
+		
+		while(alph.length() < length)
+		{
+			int rand = Util.getRandomNumberInRange(0, 25);
+			if(!alph.contains(alphabet.get(rand)))
+				alph += alphabet.get(rand);
+		}
+		int index = Util.getRandomNumberInRange(0, alph.length()-1);
+		String let = alph.substring(index, index+1);
+		
+		int numLetters = Util.getRandomNumberInRange(2, 6);
+		list.add(new CustomNumber(let));
+		list.add(new Int(numLetters));
+		list.add(new CustomNumber(alph));
+		
+		int answer = 0;
+		
+		while(numLetters >0)
+		{
+			answer += numLetters* (int) Math.pow(length, numLetters-1);
+			numLetters--;
+		}
+		ans = new Int(answer);
 	}
 
 	private void gen3() {
+		int num = Util.getRandomNumberInRange(3, 6);
+		list.add(new Int(num));
+		ans = new Int(num);
 
 	}
 
--- src/BasicCountingTechniques/Casework/Question.java
+++ src/BasicCountingTechniques/Casework/Question.java
@@ -5,8 +5,9 @@ import util.lang.LangUtil;
 public class Question extends com.Question {
 	private static String[] questions = new String[9];
 	static {
-		questions[0] = "On the island of [civilization_0], the [civilization_0] alphabet has only [num_0] letters, [num_1], and every word in the [civilization_1] has no more than [num_2] letters in it. How many words are possible?";
-		questions[1] = "The Mumblians decide to change their laws to only allow words that contain the letter A at least once. But they still limit their words to 3 letters or less, and they wish to use only letters AEMBN.";
+		questions[0] = "On the island of [civilization_0], the [civilization_0] alphabet has only [num_0] letters, [num_1], and every word in the [civilization_0] language has no more than [num_2] letters in it. How many words are possible?";
+		questions[1] = "The [civilization_0] decide to change their laws to only allow words that contain the letter [num_0] at least once. But they still limit their words to [num_1] letters or less, and they wish to use only letters [num_2].";
+		questions[2] = "At a [foodshop_0] shop, you can order your [toppings_0] [foodshop_0_pl] in two sizes, with or without [toppings_1_pl], and you get your choice of [num_0] different types of [toppings_2_pl]. How many different [toppings_0] [foodshop_0_pl] are there?";
 	}
 
 	public String getQ(com.Context _c) {
