ae4d289d4ffb19ea271e4ed8f1bb01feb3c9fefe
Douglass Boshkoff <dboshkoff@parktudor.org>
2014-06-27 10:30:29 -0400

    sync

--- src/BasicCountingTechniques/ConstructiveCounting/Question.java
+++ src/BasicCountingTechniques/ConstructiveCounting/Question.java
@@ -15,7 +15,7 @@ public class Question extends com.Question {
 	questions[2] = "How many sequences, x1, x2, x3, … , x[num_2] can be formed in which all the xi integers greater than [num_0] and less than [num_1], and no two adjacent xi are equal?";
 	questions[3] = "In how many ways can we choose a group of 3 different numbers from the group 1, 2, 3, … , [num_0] such that one number is the average of the other two? (The order in which we choose the numbers does not matter.)";
 	questions[4] = "How many license plates can be formed if every license plate has [num_0] different letters followed by [num_1] different digits?";
-	questions[5] = "How many 3 digit numbers have the property that the first digit is at least twice the second digit?";
+	questions[5] = "How many 3 digit numbers have the property that the [num_0] digit is at least twice the [num_1] digit?";
 	questions[6] = "How many positive, even 3-digit numbers exist such that the sum of the hundreds digit and the tens digit equals the units digit?";
 	questions[7] = "How many 4 digit numbers have the second digit even and the fourth digit at least twice the second digit?";
 	questions[8] = "How many 4 digit number have the property that the last digit is equal to the sum of the first 2 digits?";
@@ -57,6 +57,9 @@ public class Question extends com.Question {
 		}
 		else if(c.getType() == 5)
 		{
+			hs.put(0, "third");
+			hs.put(1, "second");
+			hs.put(2, "first");
 			if(chance == 1)
 			{
 				return LangUtil.populate(questions[5],c.getList());
